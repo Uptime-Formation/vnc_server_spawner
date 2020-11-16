438d8c995f519203e255b96fa1f4bfa5e3ce079b
Hadrien <26697460+ketsapiwiq@users.noreply.github.com>
2020-11-16 10:45:18 +0100

    more variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,29 @@
 variable "stagiaires" {
-  default = [{
+  default = [
+    {
     name= "demo", # keep this to show the world the power of guacamole
     password="devops101"
-  }]
+  },
+    {
+    name= "stagiaire1", # keep this to show the world the power of guacamole
+    password="stagiaire1_passwd"
+  },
+    {
+    name= "stagiaire2", # keep this to show the world the power of guacamole
+    password="stagiaire2_passwd"
+  }
+  ]
 }
 
 variable "formateurs" {
-  default = [{
+  default = [
+    {
     name= "hadrien",
+    password="hadrien_devops202"
+  },
+    {
+    name= "elie",
     password="devops202"
-  }]
+  },
+  ]
 }
