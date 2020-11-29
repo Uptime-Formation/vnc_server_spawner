ec5fffb4fe6318321da5b5803d9b267a17067479
Elie Gavoty <eliegavoty@free.fr>
2020-11-29 18:19:00 +0100

    reswitch to hcloud vps + digitalocean DNS

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,47 +3,51 @@ variable "scaleway_api_secret_key" {}
 variable "scaleway_api_access_key" {}
 variable "scaleway_orga_id" {}
 
-variable "ovh_application_key" {}
-variable "ovh_application_secret" {}
-variable "ovh_consumer_key" {}
+variable "digitalocean_token" {}
 
-module "servers" {
-  source = "./servers_providers/scaleway"
+variable "hcloud_token" {}
 
-  scaleway_api_secret_key = var.scaleway_api_secret_key
-  scaleway_api_access_key = var.scaleway_api_access_key
-  scaleway_orga_id        = var.scaleway_orga_id
-  stagiaires_names        = var.stagiaires_names
-  formateurs_names        = var.formateurs_names
-}
+# variable "ovh_application_key" {}
+# variable "ovh_application_secret" {}
+# variable "ovh_consumer_key" {}
 
 # module "servers" {
-#   source = "./servers_providers/hcloud"
+#   source = "./servers_providers/scaleway"
 
-#   hcloud_token = var.hcloud_token
-#   stagiaires_names = var.stagiaires_names
-#   formateurs_names = var.formateurs_names
+#   scaleway_api_secret_key = var.scaleway_api_secret_key
+#   scaleway_api_access_key = var.scaleway_api_access_key
+#   scaleway_orga_id        = var.scaleway_orga_id
+#   stagiaires_names        = var.stagiaires_names
+#   formateurs_names        = var.formateurs_names
 # }
 
+module "servers" {
+  source = "./servers_providers/hcloud"
 
-module "domains" {
-  source                    = "./domains_providers/ovh"
-  ovh_application_key       = var.ovh_application_key
-  ovh_application_secret    = var.ovh_application_secret
-  ovh_consumer_key          = var.ovh_consumer_key
-  stagiaires_names          = var.stagiaires_names
-  formateurs_names          = var.formateurs_names
-  vnc_stagiaires_public_ips = module.servers.vnc_stagiaires_public_ips
-  vnc_formateurs_public_ips = module.servers.vnc_formateurs_public_ips
-  guacamole_public_ip       = module.servers.guacamole_public_ip
+  hcloud_token = var.hcloud_token
+  stagiaires_names = var.stagiaires_names
+  formateurs_names = var.formateurs_names
 }
 
+
 # module "domains" {
-#   source                    = "./domains_providers/digital_ocean"
-#   digitalocean_token        = var.digitalocean_token
+#   source                    = "./domains_providers/ovh"
+#   ovh_application_key       = var.ovh_application_key
+#   ovh_application_secret    = var.ovh_application_secret
+#   ovh_consumer_key          = var.ovh_consumer_key
 #   stagiaires_names          = var.stagiaires_names
 #   formateurs_names          = var.formateurs_names
 #   vnc_stagiaires_public_ips = module.servers.vnc_stagiaires_public_ips
 #   vnc_formateurs_public_ips = module.servers.vnc_formateurs_public_ips
 #   guacamole_public_ip       = module.servers.guacamole_public_ip
 # }
+
+module "domains" {
+  source                    = "./domains_providers/digital_ocean"
+  digitalocean_token        = var.digitalocean_token
+  stagiaires_names          = var.stagiaires_names
+  formateurs_names          = var.formateurs_names
+  vnc_stagiaires_public_ips = module.servers.vnc_stagiaires_public_ips
+  vnc_formateurs_public_ips = module.servers.vnc_formateurs_public_ips
+  guacamole_public_ip       = module.servers.guacamole_public_ip
+}
