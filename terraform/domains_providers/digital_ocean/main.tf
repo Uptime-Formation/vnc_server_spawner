da183512415e5a768b4d2af9a7977f1c872ed7a7
Elie Gavoty <eliegavoty@free.fr>
2020-11-29 18:18:33 +0100

    update to terraform 0.13 new provider mgmt

--- terraform/domains_providers/digital_ocean/main.tf
+++ terraform/domains_providers/digital_ocean/main.tf
@@ -7,6 +7,15 @@ variable "vnc_stagiaires_public_ips" {}
 variable "vnc_formateurs_public_ips" {}
 variable "guacamole_public_ip" {}
 
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "2.2.0"
+    }
+  }
+}
+
 provider "digitalocean" {
   token = var.digitalocean_token
 }
