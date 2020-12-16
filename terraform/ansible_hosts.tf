92c5c7487c8215e26c9c06fc449a57b102fdd968
Elie Gavoty <eliegavoty@free.fr>
2020-12-16 14:23:01 +0100

    vnc wireguard+ufw security

--- terraform/ansible_hosts.tf
+++ terraform/ansible_hosts.tf
@@ -19,29 +19,32 @@ provider "ansible" {
 resource "ansible_host" "ansible_vnc_servers" {
   count = length(module.servers.vnc_stagiaires_public_ips)
   inventory_hostname = "vnc-${element(var.stagiaires_names, count.index)}"
-  groups = ["all", "scaleway", "vnc_servers", "vnc_servers_stagiaires"]
+  groups = ["all", "scaleway", "vnc_servers", "vnc_servers_stagiaires", "wireguard"]
   vars = {
     ansible_host = element(module.servers.vnc_stagiaires_public_ips, count.index)
     username = element(var.stagiaires_names, count.index)
+    vpn_ip = "10.111.0.1${count.index}"
   }
 }
 
 resource "ansible_host" "ansible_vnc_servers_formateur" {
   count = length(var.formateurs_names)
   inventory_hostname = "vnc-formateur-${element(var.formateurs_names, count.index)}"
-  groups = ["all", "scaleway", "vnc_servers", "vnc_servers_formateur"]
+  groups = ["all", "scaleway", "vnc_servers", "vnc_servers_formateur", "wireguard"]
   vars = {
     ansible_host = element(module.servers.vnc_formateurs_public_ips, count.index)
     username = element(var.formateurs_names, count.index)
+    vpn_ip = "10.111.0.2${count.index}"
   }
 }
 
 resource "ansible_host" "ansible_guacamole_server" {
   inventory_hostname = "guacamole-server"
-  groups = ["all", "scaleway", "guacamole_servers"]
+  groups = ["all", "scaleway", "guacamole_servers", "wireguard"]
   vars = {
     ansible_host = module.servers.guacamole_public_ip
     guacamole_domain = module.domains.guacamole_domain
+    vpn_ip = "10.111.0.1"
   }
 }
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,7 @@
 variable "stagiaires_names" {
   default = [
-    "aleks",
-    "manu",
-    "marine",
-    "vincent",
-    # "alexandrechoppe", # keep this to show the world the power of guacamole
-    # "fabricenizet", # keep this to show the world the power of guacamole
-    # "christophegiliberto", # keep this to show the world the power of guacamole
-    # "yohanndion", # keep this to show the world the power of guacamole
-    # "florianmaire", # keep this to show the world the power of guacamole
-    # "ghislaingrandjean", # keep this to show the world the power of guacamole
-    # "brunofrederic", # keep this to show the world the power of guacamole
-    # "amandinegodard", # keep this to show the world the power of guacamole
-    # "juliengarciacolon", # keep this to show the world the power of guacamole
-    # "gaellenowak", # keep this to show the world the power of guacamole
+    "stagiaire1",
+    "stagiaire2",
   ]
 }
 
