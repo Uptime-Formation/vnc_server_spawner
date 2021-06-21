ae46509d8df095f45478a8808409e7136286bc56
Elie Gavoty <eliegavoty@free.fr>
2021-06-21 12:22:48 +0200

    update vnc localhost install and add script

---
+++ install_vnc_localhost.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eu
+
+sudo apt update && sudo apt upgrade
+
+sudo apt install -y git ansible wireguard openssh-server
+
+
+# get your wg1.conf first from the teacher, put it in /etc/wireguard then
+sudo systemctl start wg-quick@wg1.service
+sudo systemctl enable wg-quick@wg1.service
+
+cd ansible
+
+# In host_vars/localhost change stagiaire (2 times) by your user name then:
+sudo ansible-playbook playbooks/vnc_localhost.yml
